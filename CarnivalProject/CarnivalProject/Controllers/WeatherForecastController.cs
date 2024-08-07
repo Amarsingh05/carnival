8eadc8165089b7eca26a592cb56c120fcece4426
AmarsinghPardeshi <amarsingh.pardeshi@skosystems.com>
2024-08-07 12:12:48 +0530

    refactoring project names

--- CarnivalProject/CarnivalProject/Controllers/WeatherForecastController.cs
+++ CarnivalProject/CarnivalProject/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication1.Controllers
+namespace CarnivalProject.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- CarnivalProject/CarnivalProject/WeatherForecast.cs
+++ CarnivalProject/CarnivalProject/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1
+namespace CarnivalProject
 {
     public class WeatherForecast
     {
